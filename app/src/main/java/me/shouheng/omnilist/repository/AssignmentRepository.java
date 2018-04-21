35936d9e7632902ee60778e57e194766d3272cb0
WngShhng <shouheng2015@gmail.com>
2018-04-21 21:59:27 +0800

    assignments repo

--- app/src/main/java/me/shouheng/omnilist/repository/AssignmentRepository.java
+++ app/src/main/java/me/shouheng/omnilist/repository/AssignmentRepository.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.MutableLiveData;
 import java.util.List;
 
 import me.shouheng.omnilist.async.NormalAsyncTask;
+import me.shouheng.omnilist.config.Constants;
 import me.shouheng.omnilist.model.Assignment;
 import me.shouheng.omnilist.model.Category;
 import me.shouheng.omnilist.model.data.Resource;
@@ -13,12 +14,30 @@ import me.shouheng.omnilist.model.enums.Status;
 import me.shouheng.omnilist.provider.AssignmentsStore;
 import me.shouheng.omnilist.provider.BaseStore;
 import me.shouheng.omnilist.provider.schema.AssignmentSchema;
+import me.shouheng.omnilist.provider.schema.BaseSchema;
+import me.shouheng.omnilist.utils.TimeUtils;
+import me.shouheng.omnilist.utils.preferences.SearchPreferences;
 
 public class AssignmentRepository extends BaseRepository<Assignment> {
 
     @Override
     protected BaseStore<Assignment> getStore() {
         return AssignmentsStore.getInstance();
+    }
+
+    public LiveData<Resource<List<Assignment>>> getAssignments(String queryString, String whereSQL) {
+        MutableLiveData<Resource<List<Assignment>>> result = new MutableLiveData<>();
+        new NormalAsyncTask<>(result, () -> getStore().get(getQueryConditions(queryString), whereSQL)).execute();
+        return result;
+    }
+
+    private String getQueryConditions(String queryString) {
+        SearchPreferences searchPreferences = SearchPreferences.getInstance();
+        return (searchPreferences.isTagsIncluded() ?
+                " ( " + AssignmentSchema.NAME + " LIKE '%'||'" + queryString + "'||'%' " + " OR " + AssignmentSchema.TAGS + " LIKE '%'||'" + queryString + "'||'%' ) " : AssignmentSchema.NAME + " LIKE '%'||'" + queryString + "'||'%'")
+                + (searchPreferences.isArchivedIncluded() ? "" : " AND " + BaseSchema.STATUS + " != " + Status.ARCHIVED.id)
+                + (searchPreferences.isTrashedIncluded() ? "" : " AND " + BaseSchema.STATUS + " != " + Status.TRASHED.id)
+                + " AND " + BaseSchema.STATUS + " != " + Status.DELETED.id;
     }
 
     public LiveData<Resource<List<Assignment>>> getAssignments(Category category, Status status, boolean includeCompleted) {
@@ -52,6 +71,21 @@ public class AssignmentRepository extends BaseRepository<Assignment> {
     public LiveData<Resource<List<Assignment>>> getAssignments(long startMillis, long endMillis) {
         MutableLiveData<Resource<List<Assignment>>> result = new MutableLiveData<>();
         new NormalAsyncTask<>(result, () -> ((AssignmentsStore) getStore()).getAssignments(startMillis, endMillis)).execute();
+        return result;
+    }
+
+    /**
+     * Get all assignments to display fot today fragment: overdue assignments and those start today.
+     *
+     * @return the assignments */
+    public LiveData<Resource<List<Assignment>>> getToday() {
+        MutableLiveData<Resource<List<Assignment>>> result = new MutableLiveData<>();
+        new NormalAsyncTask<>(result, () -> {
+            long todayEnd = TimeUtils.endToday().getTime();
+            return getStore().get(AssignmentSchema.START_TIME + " <= " + todayEnd +
+                    " AND " + AssignmentSchema.PROGRESS + " != " + Constants.MAX_ASSIGNMENT_PROGRESS,
+                    null, Status.NORMAL, false);
+        }).execute();
         return result;
     }
 }
