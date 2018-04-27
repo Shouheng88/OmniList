142eff581afefbe4dedbaff1decc160e139cf09b
WngShhng <shouheng2015@gmail.com>
2018-04-27 22:17:21 +0800

    setup user preferences fragment

--- app/src/main/java/me/shouheng/omnilist/fragment/setting/SettingsPreferences.java
+++ app/src/main/java/me/shouheng/omnilist/fragment/setting/SettingsPreferences.java
@@ -1,14 +1,25 @@
 package me.shouheng.omnilist.fragment.setting;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import me.shouheng.omnilist.PalmApp;
 import me.shouheng.omnilist.R;
 import me.shouheng.omnilist.listener.OnFragmentDestroyListener;
+import me.shouheng.omnilist.utils.ToastUtils;
+import me.shouheng.omnilist.utils.preferences.UserPreferences;
 
 public class SettingsPreferences extends BaseFragment {
+
+    private UserPreferences userPreferences;
+
+    private Preference prefVideo;
 
     public static SettingsPreferences newInstance() {
         Bundle args = new Bundle();
@@ -21,9 +32,18 @@ public class SettingsPreferences extends BaseFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        userPreferences = UserPreferences.getInstance();
+
         configToolbar();
 
-        addPreferencesFromResource(R.xml.preferences_notification);
+        addPreferencesFromResource(R.xml.preferences_preferences);
+
+        prefVideo = findPreference(R.string.key_video_size_limit);
+        prefVideo.setOnPreferenceClickListener(preference -> {
+            showVideoLimitEditor();
+            return true;
+        });
+        updateVideoSizePref();
     }
 
     private void configToolbar() {
@@ -31,6 +51,35 @@ public class SettingsPreferences extends BaseFragment {
         if (actionBar != null) {
             actionBar.setTitle(R.string.setting_preferences);
         }
+    }
+
+    private void showVideoLimitEditor() {
+        new MaterialDialog.Builder(getActivity())
+                .title(R.string.video_limit)
+                .content(R.string.setting_video_limit_size)
+                .inputType(InputType.TYPE_CLASS_NUMBER)
+                .inputRange(0, 2)
+                .negativeText(R.string.text_cancel)
+                .input(getString(R.string.video_limit_input_hint), String.valueOf(userPreferences.getVideoSizeLimit()),
+                        (materialDialog, charSequence) -> {
+                            try {
+                                int size = Integer.parseInt(charSequence.toString());
+                                if (size < 0) {
+                                    ToastUtils.makeToast(R.string.illegal_number);
+                                } else {
+                                    userPreferences.setVideoSizeLimit(size);
+                                    updateVideoSizePref();
+                                }
+                            } catch (Exception e) {
+                                ToastUtils.makeToast(R.string.wrong_numeric_string);
+                            }
+                        }).show();
+    }
+
+    private void updateVideoSizePref() {
+        prefVideo.setSummary(String.format(
+                PalmApp.getStringCompact(R.string.setting_video_limit_size_sub),
+                userPreferences.getVideoSizeLimit()));
     }
 
     @Override
