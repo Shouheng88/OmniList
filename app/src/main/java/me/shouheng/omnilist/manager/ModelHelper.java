63ea69baab21bdc1d0eafbf9c24c10a7f83b567f
WngShhng <shouheng2015@gmail.com>
2018-04-28 23:56:50 +0800

    statistic dialog

--- app/src/main/java/me/shouheng/omnilist/manager/ModelHelper.java
+++ app/src/main/java/me/shouheng/omnilist/manager/ModelHelper.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.text.ClipboardManager;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -169,59 +175,55 @@ public class ModelHelper {
                 + location.getDistrict();
     }
 
-    // region statistic helper
-//    public static void showStatistic(Context context, Note note) {
-//        View root = LayoutInflater.from(context).inflate(R.layout.dialog_stats, null, false);
-//        LinearLayout llStats = root.findViewById(R.id.ll_stats);
-//        addStat(context, llStats, context.getString(R.string.text_created_time), TimeUtils.getPrettyTime(note.getAddedTime()));
-//        addStat(context, llStats, context.getString(R.string.text_last_modified_time), TimeUtils.getPrettyTime(note.getLastModifiedTime()));
-//        addStat(context, llStats, context.getString(R.string.text_last_sync_time), (note.getLastSyncTime().getTime() == 0 ? "--" : TimeUtils.getPrettyTime(note.getLastModifiedTime())));
-//        addStat(context, llStats, context.getString(R.string.text_chars_number), String.valueOf(note.getContent().length()));
-//        new AlertDialog.Builder(context)
-//                .setTitle(R.string.text_statistic)
-//                .setView(root)
-//                .setPositiveButton(R.string.text_confirm, null)
-//                .create()
-//                .show();
-//    }
-//
-//    private static void addStat(Context context, LinearLayout llStats, String name, String result) {
-//        LinearLayout llStat = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.item_stat, null, false);
-//        ((TextView) llStat.findViewById(R.id.tv_name)).setText(name);
-//        ((TextView) llStat.findViewById(R.id.tv_result)).setText(result);
-//        llStats.addView(llStat);
-//    }
-    // endregion
+    public static void showStatistic(Context context,
+                                     Assignment assignment,
+                                     List<SubAssignment> subAssignments,
+                                     Alarm alarm,
+                                     List<Attachment> attachments) {
+        View root = LayoutInflater.from(context).inflate(R.layout.dialog_stats, null, false);
+        LinearLayout llStats = root.findViewById(R.id.ll_stats);
 
-//    public static void showLabels(Context context, String tags) {
-//        View root = LayoutInflater.from(context).inflate(R.layout.dialog_tags, null, false);
-//        FlowLayout flowLayout = root.findViewById(R.id.fl_labels);
-//        addTagsToLayout(context, flowLayout, tags);
-//        new AlertDialog.Builder(context)
-//                .setTitle(R.string.text_tags)
-//                .setView(root)
-//                .setPositiveButton(R.string.text_confirm, null)
-//                .create()
-//                .show();
-//    }
-//
-//    private static void addTagsToLayout(Context context, FlowLayout flowLayout, String stringTags){
-//        if (TextUtils.isEmpty(stringTags)) return;
-//        String[] tags = stringTags.split(CategoryViewModel.CATEGORY_SPLIT);
-//        for (String tag : tags) addTagToLayout(context, flowLayout, tag);
-//    }
-//
-//    private static  void addTagToLayout(Context context, FlowLayout flowLayout, String tag){
-//        int margin = ViewUtils.dp2Px(context, 2f);
-//        int padding = ViewUtils.dp2Px(context, 5f);
-//        TextView tvLabel = new TextView(context);
-//        tvLabel.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-//        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(tvLabel.getLayoutParams());
-//        params.setMargins(margin, margin, margin, margin);
-//        tvLabel.setLayoutParams(params);
-//        tvLabel.setPadding(padding, 0, padding, 0);
-//        tvLabel.setBackgroundResource(ColorUtils.isDarkTheme(context) ? R.drawable.label_background_dark : R.drawable.label_background);
-//        tvLabel.setText(tag);
-//        flowLayout.addView(tvLabel);
-//    }
+        addStat(context, llStats, context.getString(R.string.text_created_time),
+                TimeUtils.getPrettyTime(assignment.getAddedTime()));
+        addStat(context, llStats, context.getString(R.string.text_last_modified_time),
+                TimeUtils.getPrettyTime(assignment.getLastModifiedTime()));
+        addStat(context, llStats, context.getString(R.string.text_last_sync_time),
+                (assignment.getLastSyncTime().getTime() == 0 ? "--" : TimeUtils.getPrettyTime(assignment.getLastModifiedTime())));
+        addStat(context, llStats, context.getString(R.string.text_alarm),
+                alarm == null ? "--" : alarm.getAlarmInfo(context));
+
+        addStat(context, llStats, context.getString(R.string.text_attachment), String.valueOf(attachments.size()));
+
+        addStat(context, llStats, context.getString(R.string.text_sub_assignment_number), String.valueOf(subAssignments.size()));
+
+        int len = 0;
+        List<String> strings = new LinkedList<>();
+        strings.add(assignment.getName());
+        strings.add(assignment.getComment());
+        for (SubAssignment subAssignment : subAssignments) {
+            strings.add(subAssignment.getContent());
+        }
+
+        for (String string : strings) {
+            if (!TextUtils.isEmpty(string)) {
+                len+=string.length();
+            }
+        }
+
+        addStat(context, llStats, context.getString(R.string.text_chars_number), String.valueOf(len));
+
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.text_statistic)
+                .setView(root)
+                .setPositiveButton(R.string.text_confirm, null)
+                .create()
+                .show();
+    }
+
+    private static void addStat(Context context, LinearLayout llStats, String name, String result) {
+        LinearLayout llStat = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.item_stat, null, false);
+        ((TextView) llStat.findViewById(R.id.tv_name)).setText(name);
+        ((TextView) llStat.findViewById(R.id.tv_result)).setText(result);
+        llStats.addView(llStat);
+    }
 }
