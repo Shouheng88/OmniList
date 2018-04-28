ed4c450b2628d51ce5fdd20bb1bab2dd9e7c60a8
WngShhng <shouheng2015@gmail.com>
2018-04-28 11:17:47 +0800

    navigate to today in month calendar fragment

--- app/src/main/java/me/shouheng/omnilist/fragment/MonthFragment.java
+++ app/src/main/java/me/shouheng/omnilist/fragment/MonthFragment.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.jeek.calendar.widget.calendar.tools.OnCalendarClickListener;
 
@@ -255,6 +259,28 @@ public class MonthFragment extends BaseFragment<FragmentMonthCalendarBinding> im
 
     public void setOnScrollListener(RecyclerView.OnScrollListener onScrollListener) {
         this.onScrollListener = onScrollListener;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.calendar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_today:
+                getBinding().mcvCalendar.focusToday();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
