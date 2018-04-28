a839635b4ed96cc9137090efc823297340387ffe
WngShhng <shouheng2015@gmail.com>
2018-04-28 23:09:10 +0800

    get markdown content of assignment

--- app/src/main/java/me/shouheng/omnilist/manager/ModelHelper.java
+++ app/src/main/java/me/shouheng/omnilist/manager/ModelHelper.java
@@ -4,21 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.Nullable;
 import android.text.ClipboardManager;
+import android.text.TextUtils;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import me.shouheng.omnilist.PalmApp;
 import me.shouheng.omnilist.R;
+import me.shouheng.omnilist.model.Alarm;
+import me.shouheng.omnilist.model.Assignment;
 import me.shouheng.omnilist.model.Attachment;
 import me.shouheng.omnilist.model.Location;
 import me.shouheng.omnilist.model.Model;
+import me.shouheng.omnilist.model.SubAssignment;
 import me.shouheng.omnilist.utils.FileHelper;
 import me.shouheng.omnilist.utils.TimeUtils;
 import me.shouheng.omnilist.utils.ToastUtils;
@@ -28,24 +30,92 @@ import me.shouheng.omnilist.utils.ToastUtils;
  * Created by wangshouheng on 2017/11/4.*/
 public class ModelHelper {
 
-    @Nullable
-    private static Pattern titlePattern;
-
-    @Nullable
-    private static Pattern imagePattern;
-
     public static <T extends Model> String getTimeInfo(T model) {
-        return PalmApp.getContext().getString(R.string.text_created_time) + " : "
+        return PalmApp.getStringCompact(R.string.text_created_time) + " : "
                 + TimeUtils.getPrettyTime(model.getAddedTime()) + "\n"
-                + PalmApp.getContext().getString(R.string.text_last_modified_time) + " : "
+                + PalmApp.getStringCompact(R.string.text_last_modified_time) + " : "
                 + TimeUtils.getPrettyTime(model.getLastModifiedTime()) + "\n"
-                + PalmApp.getContext().getString(R.string.text_last_sync_time) + " : "
+                + PalmApp.getStringCompact(R.string.text_last_sync_time) + " : "
                 + (model.getLastSyncTime().getTime() == 0 ? "--" : TimeUtils.getPrettyTime(model.getLastModifiedTime()));
     }
 
     public static void copyToClipboard(Activity ctx, String content) {
         ClipboardManager clipboardManager = (ClipboardManager) ctx.getSystemService(Context.CLIPBOARD_SERVICE);
         clipboardManager.setText(content);
+    }
+
+    public static String getMarkdown(Assignment assignment,
+                                     List<SubAssignment> subAssignments,
+                                     Location location,
+                                     Alarm alarm,
+                                     List<Attachment> attachments) {
+        StringBuilder sb = new StringBuilder();
+
+        // Append title
+        sb.append("## ").append(assignment.getName()).append("\n\n");
+
+        // Time info
+        sb.append(PalmApp.getStringCompact(R.string.text_created_time))
+                .append(": ")
+                .append(TimeUtils.getPrettyTime(assignment.getAddedTime()))
+                .append("\n");
+        sb.append(PalmApp.getStringCompact(R.string.text_last_modified_time))
+                .append(": ")
+                .append(TimeUtils.getPrettyTime(assignment.getLastModifiedTime()))
+                .append("\n");
+        sb.append(PalmApp.getStringCompact(R.string.text_last_sync_time))
+                .append(": ")
+                .append(assignment.getLastSyncTime().getTime() == 0 ? "--" : TimeUtils.getPrettyTime(assignment.getLastModifiedTime()))
+                .append("\n");
+
+        // Append notification
+        if (alarm != null) {
+            sb.append(PalmApp.getStringCompact(R.string.text_notification))
+                    .append(": ")
+                    .append(alarm.getAlarmInfo(PalmApp.getContext()))
+                    .append("\n\n");
+        }
+
+        // Append location
+        if (location != null) {
+            sb.append(PalmApp.getStringCompact(R.string.text_location_info))
+                    .append(": ")
+                    .append(getFormatLocation(location))
+                    .append("\n\n");
+        }
+
+        // Append sub assignment
+        for (SubAssignment subAssignment : subAssignments) {
+            switch (subAssignment.getSubAssignmentType()) {
+                case TODO:
+                    sb.append(subAssignment.isCompleted() ? "- [x] " : "- [ ] ").append(subAssignment.getContent()).append("\n\n");
+                    break;
+                case NOTE:
+                    sb.append(subAssignment.getContent()).append("\n\n");
+                    break;
+            }
+        }
+
+        // Append comment
+        if (!TextUtils.isEmpty(assignment.getComment())) {
+            sb.append(PalmApp.getStringCompact(R.string.text_comments))
+                    .append(": ")
+                    .append(assignment.getComment()).append("\n\n");
+        }
+
+        // Append attachment
+        if (!attachments.isEmpty()) {
+            sb.append("------")
+                    .append("\n")
+                    .append(PalmApp.getStringCompact(R.string.text_attachment))
+                    .append(": ");
+            for (Attachment attachment : attachments) {
+                sb.append("![").append(attachment.getName()).append("]")
+                        .append("(").append(attachment.getUri().toString()).append(")").append("\n");
+            }
+        }
+
+        return sb.toString();
     }
 
     public static void share(Context context, String title, String content, List<Attachment> attachments) {
@@ -71,8 +141,7 @@ public class ModelHelper {
         shareIntent.putExtra(Intent.EXTRA_SUBJECT, title);
         shareIntent.putExtra(Intent.EXTRA_TEXT, content);
 
-        context.startActivity(Intent.createChooser(shareIntent,
-                context.getResources().getString(R.string.share_message_chooser)));
+        context.startActivity(Intent.createChooser(shareIntent, PalmApp.getStringCompact(R.string.share_message_chooser)));
     }
 
     public static void shareFile(Context context, File file, String mimeType) {
@@ -80,8 +149,7 @@ public class ModelHelper {
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType(mimeType);
         shareIntent.putExtra(Intent.EXTRA_STREAM, FileHelper.getUriFromFile(context, file));
-        context.startActivity(Intent.createChooser(shareIntent,
-                context.getResources().getString(R.string.share_message_chooser)));
+        context.startActivity(Intent.createChooser(shareIntent, PalmApp.getStringCompact(R.string.share_message_chooser)));
     }
 
     public static <T extends Model> void copyLink(Activity ctx, T model) {
